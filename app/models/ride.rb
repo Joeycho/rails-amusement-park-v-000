87cea9011debaa8119e3e6a00ec63c6334e153ac
Joey Cho <cjy12182000@naver.com>
2019-03-01 13:25:32 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -17,7 +17,7 @@ class Ride < ActiveRecord::Base
         self.user.nausea+=self.attraction.nausea_rating
         self.user.happiness+=self.attraction.happiness_rating
         self.user.save
-        flash[:message] = "Thanks for riding the #{self.user.name}!"
+        errors.add(:message, "Thanks for riding the #{self.user.name}!")
       end
     end
 end
